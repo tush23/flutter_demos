663989f7768d598b913ce78e62dbd46fcd2cf095
tushardubey <dubey.tushar23@gmail.com>
2021-01-21 10:54:41 +0530

    rem

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,6 @@ class MyApp extends StatelessWidget {
   }
 }
 // class MyAppwithNemo extends StatelessWidget {
-//   // This widget is the root of your application.
 //   @override
 //   Widget build(BuildContext context) {
 //     return NeumorphicApp(
